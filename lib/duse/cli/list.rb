7e986a292d899d1124217b0e7a5edb0f48c45b3e
flower-pot <flower-pot@flowerpot.io>
2014-12-17 19:09:47 +0100

    justify listing secrets

--- lib/duse/cli/list.rb
+++ lib/duse/cli/list.rb
@@ -6,8 +6,9 @@ module Duse
       def run
         Duse.session = Duse::Client::Session.new(uri: CLIConfig.uri, token: CLIConfig.token)
         secrets = Duse::Secret.all
+        number  = secrets.length + 1
         secrets.each_with_index do |s, i|
-          puts "#{i+1}: #{s.title}"
+          puts "#{(i+1).to_s.rjust(number.to_s.length)}: #{s.title}"
         end
       end
     end
