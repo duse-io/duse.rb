3ff8e7265eba40c0ddf980f91a38455a6ab57fc3
flower-pot <flower-pot@flowerpot.io>
2015-02-08 21:46:52 +0100

    add plain flag when getting a secret

--- lib/duse/cli/get_secret.rb
+++ lib/duse/cli/get_secret.rb
@@ -6,6 +6,8 @@ module Duse
     class GetSecret < ApiCommand
       description 'Retrieve a secret'
 
+      on('-p', '--plain', 'Print the decrypted secret plain, without additional information.')
+
       def run(*arguments)
         secret_id = arguments.shift unless arguments.empty?
         secret_id ||= terminal.ask('Secret to retrieve: ').to_i
@@ -13,10 +15,17 @@ module Duse
         secret      = Duse::Secret.find secret_id
         private_key = OpenSSL::PKey::RSA.new File.read File.expand_path '~/.ssh/id_rsa'
 
-        puts ""
-        puts "Name:   #{secret.title}"
-        puts "Secret: #{secret.decrypt(private_key)}"
-        puts "Access: #{secret.users.map(&:username).join(', ')}"
+        if plain?
+          say secret.decrypt(private_key)
+          return
+        end
+
+        say <<-SECRET_DESCRIPTION.gsub /^( |\t)+/, ""
+
+          Name:   #{secret.title}
+          Secret: #{secret.decrypt(private_key)}
+          Access: #{secret.users.map(&:username).join(', ')}
+        SECRET_DESCRIPTION
       end
 
       def self.command_name
