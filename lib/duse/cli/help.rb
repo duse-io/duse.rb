74b82176d9760ecb6f86aa067d10e460524f9391
flower-pot <flower-pot@flowerpot.io>
2015-03-04 17:32:00 +0100

    extract command traversal

--- lib/duse/cli/help.rb
+++ lib/duse/cli/help.rb
@@ -7,19 +7,21 @@ module Duse
 
       def run(*args)
         unless args.empty?
-          command_class = CLI.command(args.shift)
-          loop do
-            unless command_class.subcommands.key? args.first
-              break
-            end
-            command_class = command_class.subcommands[args.shift]
-          end
-          say command_class.new.help
+          say command(args).new.help
         else
           say "Usage: duse COMMAND ...\n\nAvailable commands:\n\n"
           commands.each { |c| say "\t#{color(c.command_name, :command).ljust(22)} #{color(c.description, :info)}" }
           say "\nrun `#$0 help COMMAND` for more infos"
         end
+      end
+
+      def command(command_chain)
+        command_class = CLI.command(command_chain.shift)
+        loop do
+          break unless command_class.subcommands.key? command_chain.first
+          command_class = command_class.subcommands[command_chain.shift]
+        end
+        command_class
       end
 
       def commands
