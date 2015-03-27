3b161049aa0594235aae25210225b15783dd8b61
flower-pot <fbranczyk@gmail.com>
2015-03-27 20:04:09 +0100

    make sure to cleanup filesystem
    fixes #5

--- spec/cli/commands/config_spec.rb
+++ spec/cli/commands/config_spec.rb
@@ -2,6 +2,17 @@ require 'duse/cli/config'
 require 'fakefs/safe'
 
 describe Duse::CLI::Config do
+  include FakeFS::SpecHelpers
+
+  before :each do
+    @dir = File.dirname(Duse::CLIConfig.config_file)
+    FileUtils.mkdir_p @dir
+  end
+
+  after :each do
+    FileUtils.rm_rf @dir
+  end
+
   it 'should handle a false uri correctly' do
     expect(run_cli('config') { |i| i.puts('test') }.err).to eq(
       "Not an uri\n"
@@ -9,11 +20,8 @@ describe Duse::CLI::Config do
   end
 
   it 'should process a correct uri' do
-    FakeFS do
-      FileUtils.mkdir_p(File.dirname(Duse::CLIConfig.config_file))
-      run = run_cli('config') { |i| i.puts('https://duse.io/') }
-      expect(run.err).to eq('')
-      expect(run.success?).to be true
-    end
+    run = run_cli('config') { |i| i.puts('https://duse.io/') }
+    expect(run.err).to eq('')
+    expect(run.success?).to be true
   end
 end
