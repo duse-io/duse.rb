852efaeeda859e71edc2d56d17ebb85cc92e3469
flower-pot <fbranczyk@gmail.com>
2015-05-02 19:27:27 +0200

    show the list of secrets to choose from

--- lib/duse/cli/secret_get.rb
+++ lib/duse/cli/secret_get.rb
@@ -11,7 +11,13 @@ module Duse
       on('-p', '--plain', 'Print the decrypted secret plain, without additional information.')
 
       def run(secret_id = nil)
-        secret_id ||= terminal.ask('Secret to retrieve: ').to_i
+        if secret_id.nil?
+          secrets = Duse::Secret.all
+          secrets.each do |s|
+            say "#{s.id}: #{s.title}"
+          end
+          secret_id = terminal.ask("\nSelect the id of the secret to retrieve: ").to_i
+        end
 
         secret = Duse::Secret.find secret_id
         print_secret(secret)
