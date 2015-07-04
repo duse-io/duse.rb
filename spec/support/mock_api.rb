37f24773f4450e5e6c36803f8f579bc5c8ab49c8
flower-pot <fbranczyk@gmail.com>
2015-07-04 10:08:57 +0200

    adapt all webmocks with api mock wrapper
    this now allows the mocking framework to be exchanged more easily

--- spec/support/mock_api.rb
+++ spec/support/mock_api.rb
@@ -65,25 +65,21 @@ module MockAPI
       'email' => 'flower-pot@example.org',
       'public_key' => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmMm3Ovh7gU0rLHK4NiHh\nWaYRrV9PH6XtHqV0GoiHH7awrjVkT1aZiS+nlBxckfuvuQjRXakVCZh18UdQadVQ\n7FLTWMZNoZ/uh41g4Iv17Wh1I3Fgqihdm83cSWvJ81qQCVGBaKeVitSa49zT/Mmo\noBvYFwulaqJjhqFc3862Rl3WowzGVqGf+OiYhFrBbnIqXijDmVKsbqkG5AILGo1n\nng06HIAvMqUcGMebgoju9SuKaR+C46KT0K5sPpNw/tNcDEZqZAd25QjAroGnpRHS\nI9hTEuPopPSyRqz/EVQfbhi0LbkdDW9S5ECw7GfFPFpRp2239fjl/9ybL6TkeZL7\nAwIDAQAB\n-----END PUBLIC KEY-----\n",
       'url' => 'https://example.com/users/2'
-    }.to_json
+    }
 
-    stub_request(:get, "https://example.com/users/#{id}").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 200, body: payload)
+    stub_get("/users/#{id}").body(payload).activate
   end
 
   def stub_user_get1(id = 1)
     payload = {
-        'id' => 1,
-        'username' => 'server',
-        'email' => 'server@localhost',
-        'public_key' => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvyvyAf7lnVx9eQcAS7JL\nYRHrqJJe51rAdanaUiiy8eek2Iyh6JG551EK7x4n9/Y7r0fW2sNmy+Bp3FpL8E/p\ncxutggTWCnUQUvXmEEm5qZ1KOIIlEQNp5glToAenJ7pxotJsTMlVw4tizsKScenc\n8w+02wpcmWuzWKjoY/G5KV33UDz/LxVo1RJdJp94JiL/OinIl+uk+Vf7VZj/E8g/\n7DyXIuiBosVpj9E9T4kpxs3/7RmUfDzUisVq0UvgflRjvP1V+1KdpNnjVB+H08mb\nSVO6yf2YOcrPDRa3pgz7PIr225QJ+HmVjPTg5VAy7rUxhCK+q+HNd2oz35zA70SO\npQIDAQAB\n-----END PUBLIC KEY-----\n",
-        'url' => 'https://example.com/users/1'
-    }.to_json
+      'id' => 1,
+      'username' => 'server',
+      'email' => 'server@localhost',
+      'public_key' => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvyvyAf7lnVx9eQcAS7JL\nYRHrqJJe51rAdanaUiiy8eek2Iyh6JG551EK7x4n9/Y7r0fW2sNmy+Bp3FpL8E/p\ncxutggTWCnUQUvXmEEm5qZ1KOIIlEQNp5glToAenJ7pxotJsTMlVw4tizsKScenc\n8w+02wpcmWuzWKjoY/G5KV33UDz/LxVo1RJdJp94JiL/OinIl+uk+Vf7VZj/E8g/\n7DyXIuiBosVpj9E9T4kpxs3/7RmUfDzUisVq0UvgflRjvP1V+1KdpNnjVB+H08mb\nSVO6yf2YOcrPDRa3pgz7PIr225QJ+HmVjPTg5VAy7rUxhCK+q+HNd2oz35zA70SO\npQIDAQAB\n-----END PUBLIC KEY-----\n",
+      'url' => 'https://example.com/users/1'
+    }
 
-    stub_request(:get, "https://example.com/users/#{id}").
-        with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-        to_return(status: 200, body: payload)
+    stub_get("/users/#{id}").body(payload).activate
   end
 
   def stub_server_user_get
@@ -98,10 +94,9 @@ module MockAPI
       'email' => 'adracus@example.org',
       'public_key' => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0Y1b9awjW0nshQXk64uO\n1v+GYliBH8ogu6QjQDn0eoLIfcOibrotbhJuSS0G46yOhboOCZQWrwyqi4MYtTMB\nH3ITTmNkhzOkdRXLJGJXXv3OCYR0J+PdCXbrtfYkvqOgyJE4RAR6YBEO/XcQk0Em\nE4IDFq22Aar7MxSjrLk17LX9mTifdzg1xdxX5myX4NrXGVWTWKeS5klLWCe9AigQ\n35b8c2Zyehx6jxHk+jt5CguMC9VqSyJobKdu926W4k2AgzWRdZh0EvCg2wWjlYjc\nhJEnrpHLeJxGMEThPoGqgQWiG5BBYIl9kx1vg1QZmS2biS6djGpGIn8l8PN30+QS\n5QIDAQAB\n-----END PUBLIC KEY-----\n",
       'url' => 'https://example.com/users/3'
-    }.to_json
-    stub_request(:get, "https://example.com/users/3").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 200, body: payload, headers: {})
+    }
+
+    stub_get('/users/3').body(payload).activate
   end
 
   def stub_get_secrets
@@ -109,10 +104,8 @@ module MockAPI
       'id' => 1,
       'title' => 'test',
       'url' => 'http://example.com/secrets/1'
-    }].to_json
-    stub_request(:get, "https://example.com/secrets").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 200, body: payload)
+    }]
+    stub_get('/secrets').body(payload).activate
   end
 
   def stub_secret_get_broken_signature
@@ -138,11 +131,9 @@ module MockAPI
         'url' => 'https://example.com/users/2'
       }],
       'url' => 'http://example.com/secrets/1'
-    }.to_json
+    }
 
-    stub_request(:get, "https://example.com/secrets/2").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 200, body: payload)
+    stub_get('/secrets/2').body(payload).activate
   end
 
   def stub_secret_get
@@ -168,17 +159,13 @@ module MockAPI
         'url' => 'https://example.com/users/2'
       }],
       'url' => 'http://example.com/secrets/1'
-    }.to_json
+    }
 
-    stub_request(:get, "https://example.com/secrets/1").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 200, body: payload)
+    stub_get('/secrets/1').body(payload).activate
   end
 
   def stub_secret_delete
-    stub_request(:delete, "https://example.com/secrets/1").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 204, body: "", headers: {})
+    stub_delete('/secrets/1').activate
   end
 
   def stub_create_secret
@@ -206,9 +193,7 @@ module MockAPI
       'url' => 'http://example.com/secrets/1'
     }.to_json
 
-    stub_request(:post, "https://example.com/secrets").
-      with(headers: {'Accept'=>'application/vnd.duse.1+json'}).
-      to_return(status: 201, body: payload, headers: {})
+    stub_post('/secrets').body(payload).activate
   end
 end
 
