154b89a09ef4282ade2866c6ab0c41ae5dc4ccb1
flower-pot <fbranczyk@gmail.com>
2015-08-16 11:39:29 +0200

    avoid duplication in mock api

--- spec/support/mock_api.rb
+++ spec/support/mock_api.rb
@@ -36,6 +36,32 @@ module MockAPI
     end
   end
 
+  def secret_hash
+    {
+      'id' => 1,
+      'title' => 'test',
+      'cipher_text' => "0aqOigsWK04MhJ5EHgM49Q==\n",
+      'shares' => [
+        { 'last_edited_by_id' => 2, 'signature' => "dPMQbO9UzYFS4hY0U5iLjjVFTeGvDJemuEYVog6zClbLMbZG8pSaQ2MzNI1I\n0UsZJNXlyHP6NR/kTp0mpCmndrnA+c8c7UDotSE1BnMqizscZiBcTTlBn9IS\ncQsKLNsV50FDLqZnsLOuB0lvQnnUN4GrxmW38Vi/emva2voK+WQM3GybrFyd\nb2p5c9DN4abYpjASxg9IilkmnfYLfcs7LXgmtlJOl/dg7CybcaPrE+lOZn7F\nRLIfLh601cP3XyGe5WYHBMJXtJHYzczRlU/rJ98BosfqewHDjYRAHtt7IcVA\nWKseYK5TpBZLIbEK2ZcQLo65huyuKh6z4OReqjCvpQ==\n", 'content' => "NQSRqhjkHyUyT00YDgQ6WayC0EwtdytQ/iZAUdc6mnXKNQc74hae3VIJqjUf\nNkq1Qus/dS7a5DMsKa+2F74p1UF3YbswNnZ7fc2VH+iz6hIdYbgCu5bmAuVF\nXFpXk7Nw9n4Cpvjg2x5jxBQ0S1BnYY8J+EBluzichoqub4EVl5yxTWipqiaZ\nKwR1gEEKi3h5w2uljOib62gFdxUnXLtMjDlIXJjN6Kl9idCiz3upBj+4KZFx\n4qBWpvU/6xQRgbbOwfHNzTDl9RSlWSYTXvJ1/7iXYfoSEOMGJSmlGSm/P3Y/\nVPoChK0lfJYanNW4Gu4qQf9d09E5d6NQcGZdN3PNIQ==\n"  },
+        { 'last_edited_by_id' => 2, 'signature' => "GYIflo5hjcsEMjTZxnb4gsKV8bOe2XiG88u8IJ5zvJm3ZavLNmeDJ0PCKkpS\nM05NLLzbaqlGyM7Bh7TubanuvWEi412p9qyMABqVnka3co7GWE3N0RHX1JbG\ncq/O86iO3GDCA7YEwZUjdP0UhI+ytA6WNPU/hsmsDJJzaxXs5GFc51XZUBx9\nHYqKc60i1dL1UIi4IlfcvkiBfUg2lXaeJvHjj2zb0i/khpQ6q0H/OPcNE1Vn\nvYgZGN6PLy60KnirPltKynw8fii5FF2QSntINiC5GM64Eir0ZAKyh9lOQWUV\nCQvtoX4MbUXYWIZXt2sc4GzJE74mPmZ0qmD16hT/kA==\n", 'content' => "gQUlhs+uPz93UN/Cpcd7x8ochDXWbfiC6IrP7ijCfxaa9kvsVnQddYkJVgH1\nBLO6SC36b/lVsGfcvUwmO0vefGrSy2XykXVJdYPMf/LQsOukOs1rLVacg4B6\nf0KeKvLVf/HVYcKiY4L2rFIPvuWC+bUWxRThpiABSu1ZCH9qgcR24PPRnwYw\nsTT15OBHmBzhsP+b3NZI9Yh3OIzWLvDSjQmRuldryuUstPsgfWKd/xawPU4D\nJ8EUFGNltFqb+R4n2eGpNSVQCl/+o9gfMG6dq5CE5HRHwpKgXaybdy753lrF\nHWXS4g6apO4lpJS7kXdhHX7OTbA79GHj/ait9fuZLA==\n" }
+      ],
+      'users' => [{
+        'id' => 1,
+        'username' => 'server',
+        'email' => 'server@localhost',
+        'public_key' => server_public_key.to_s,
+        'url' => 'https://example.com/users/1'
+      }, {
+        'id' => 2,
+        'username' => 'flower-pot',
+        'email' => 'flower-pot@example.org',
+        'public_key' => user_public_key.to_s,
+        'url' => 'https://example.com/users/2'
+      }],
+      'url' => 'http://example.com/secrets/1'
+    }
+  end
+
   def stub_get_users
     payload = [{
       'id' => 1,
@@ -110,59 +136,15 @@ module MockAPI
   end
 
   def stub_secret_get_broken_signature
-    payload = {
-      'id' => 2,
-      'title' => 'test',
-      'cipher_text' => "0aqOigsWK04MhJ5EHgM49Q==\n",
-      'shares' => [
-        { 'last_edited_by_id' => 2, 'signature' => "blabla", 'content' => "NQSRqhjkHyUyT00YDgQ6WayC0EwtdytQ/iZAUdc6mnXKNQc74hae3VIJqjUf\nNkq1Qus/dS7a5DMsKa+2F74p1UF3YbswNnZ7fc2VH+iz6hIdYbgCu5bmAuVF\nXFpXk7Nw9n4Cpvjg2x5jxBQ0S1BnYY8J+EBluzichoqub4EVl5yxTWipqiaZ\nKwR1gEEKi3h5w2uljOib62gFdxUnXLtMjDlIXJjN6Kl9idCiz3upBj+4KZFx\n4qBWpvU/6xQRgbbOwfHNzTDl9RSlWSYTXvJ1/7iXYfoSEOMGJSmlGSm/P3Y/\nVPoChK0lfJYanNW4Gu4qQf9d09E5d6NQcGZdN3PNIQ==\n"  },
-        { 'last_edited_by_id' => 2, 'signature' => "blabla", 'content' => "gQUlhs+uPz93UN/Cpcd7x8ochDXWbfiC6IrP7ijCfxaa9kvsVnQddYkJVgH1\nBLO6SC36b/lVsGfcvUwmO0vefGrSy2XykXVJdYPMf/LQsOukOs1rLVacg4B6\nf0KeKvLVf/HVYcKiY4L2rFIPvuWC+bUWxRThpiABSu1ZCH9qgcR24PPRnwYw\nsTT15OBHmBzhsP+b3NZI9Yh3OIzWLvDSjQmRuldryuUstPsgfWKd/xawPU4D\nJ8EUFGNltFqb+R4n2eGpNSVQCl/+o9gfMG6dq5CE5HRHwpKgXaybdy753lrF\nHWXS4g6apO4lpJS7kXdhHX7OTbA79GHj/ait9fuZLA==\n" }
-      ],
-      'users' => [{
-        'id' => 1,
-        'username' => 'server',
-        'email' => 'server@localhost',
-        'public_key' => server_public_key.to_s,
-        'url' => 'https://example.com/users/1'
-      }, {
-        'id' => 2,
-        'username' => 'flower-pot',
-        'email' => 'flower-pot@example.org',
-        'public_key' => user_public_key.to_s,
-        'url' => 'https://example.com/users/2'
-      }],
-      'url' => 'http://example.com/secrets/1'
-    }
+    payload = secret_hash
+    payload['shares'][0]['signature'] = 'blabla'
+    payload['shares'][1]['signature'] = 'blabla'
 
     stub_get('/secrets/2').body(payload).activate
   end
 
   def stub_secret_get
-    payload = {
-      'id' => 1,
-      'title' => 'test',
-      'cipher_text' => "0aqOigsWK04MhJ5EHgM49Q==\n",
-      'shares' => [
-        { 'last_edited_by_id' => 2, 'signature' => "dPMQbO9UzYFS4hY0U5iLjjVFTeGvDJemuEYVog6zClbLMbZG8pSaQ2MzNI1I\n0UsZJNXlyHP6NR/kTp0mpCmndrnA+c8c7UDotSE1BnMqizscZiBcTTlBn9IS\ncQsKLNsV50FDLqZnsLOuB0lvQnnUN4GrxmW38Vi/emva2voK+WQM3GybrFyd\nb2p5c9DN4abYpjASxg9IilkmnfYLfcs7LXgmtlJOl/dg7CybcaPrE+lOZn7F\nRLIfLh601cP3XyGe5WYHBMJXtJHYzczRlU/rJ98BosfqewHDjYRAHtt7IcVA\nWKseYK5TpBZLIbEK2ZcQLo65huyuKh6z4OReqjCvpQ==\n", 'content' => "NQSRqhjkHyUyT00YDgQ6WayC0EwtdytQ/iZAUdc6mnXKNQc74hae3VIJqjUf\nNkq1Qus/dS7a5DMsKa+2F74p1UF3YbswNnZ7fc2VH+iz6hIdYbgCu5bmAuVF\nXFpXk7Nw9n4Cpvjg2x5jxBQ0S1BnYY8J+EBluzichoqub4EVl5yxTWipqiaZ\nKwR1gEEKi3h5w2uljOib62gFdxUnXLtMjDlIXJjN6Kl9idCiz3upBj+4KZFx\n4qBWpvU/6xQRgbbOwfHNzTDl9RSlWSYTXvJ1/7iXYfoSEOMGJSmlGSm/P3Y/\nVPoChK0lfJYanNW4Gu4qQf9d09E5d6NQcGZdN3PNIQ==\n"  },
-        { 'last_edited_by_id' => 2, 'signature' => "GYIflo5hjcsEMjTZxnb4gsKV8bOe2XiG88u8IJ5zvJm3ZavLNmeDJ0PCKkpS\nM05NLLzbaqlGyM7Bh7TubanuvWEi412p9qyMABqVnka3co7GWE3N0RHX1JbG\ncq/O86iO3GDCA7YEwZUjdP0UhI+ytA6WNPU/hsmsDJJzaxXs5GFc51XZUBx9\nHYqKc60i1dL1UIi4IlfcvkiBfUg2lXaeJvHjj2zb0i/khpQ6q0H/OPcNE1Vn\nvYgZGN6PLy60KnirPltKynw8fii5FF2QSntINiC5GM64Eir0ZAKyh9lOQWUV\nCQvtoX4MbUXYWIZXt2sc4GzJE74mPmZ0qmD16hT/kA==\n", 'content' => "gQUlhs+uPz93UN/Cpcd7x8ochDXWbfiC6IrP7ijCfxaa9kvsVnQddYkJVgH1\nBLO6SC36b/lVsGfcvUwmO0vefGrSy2XykXVJdYPMf/LQsOukOs1rLVacg4B6\nf0KeKvLVf/HVYcKiY4L2rFIPvuWC+bUWxRThpiABSu1ZCH9qgcR24PPRnwYw\nsTT15OBHmBzhsP+b3NZI9Yh3OIzWLvDSjQmRuldryuUstPsgfWKd/xawPU4D\nJ8EUFGNltFqb+R4n2eGpNSVQCl/+o9gfMG6dq5CE5HRHwpKgXaybdy753lrF\nHWXS4g6apO4lpJS7kXdhHX7OTbA79GHj/ait9fuZLA==\n" }
-      ],
-      'users' => [{
-        'id' => 1,
-        'username' => 'server',
-        'email' => 'server@localhost',
-        'public_key' => server_public_key.to_s,
-        'url' => 'https://example.com/users/1'
-      }, {
-        'id' => 2,
-        'username' => 'flower-pot',
-        'email' => 'flower-pot@example.org',
-        'public_key' => user_public_key.to_s,
-        'url' => 'https://example.com/users/2'
-      }],
-      'url' => 'http://example.com/secrets/1'
-    }
-
-    stub_get('/secrets/1').body(payload).activate
+    stub_get('/secrets/1').body(secret_hash).activate
   end
 
   def stub_secret_delete
@@ -170,31 +152,7 @@ module MockAPI
   end
 
   def stub_create_secret
-    payload = {
-      'id' => 1,
-      'title' => 'test',
-      'cipher_text' => "0aqOigsWK04MhJ5EHgM49Q==\n",
-      'shares' => [
-        { 'last_edited_by_id' => 2, 'signature' => "dPMQbO9UzYFS4hY0U5iLjjVFTeGvDJemuEYVog6zClbLMbZG8pSaQ2MzNI1I\n0UsZJNXlyHP6NR/kTp0mpCmndrnA+c8c7UDotSE1BnMqizscZiBcTTlBn9IS\ncQsKLNsV50FDLqZnsLOuB0lvQnnUN4GrxmW38Vi/emva2voK+WQM3GybrFyd\nb2p5c9DN4abYpjASxg9IilkmnfYLfcs7LXgmtlJOl/dg7CybcaPrE+lOZn7F\nRLIfLh601cP3XyGe5WYHBMJXtJHYzczRlU/rJ98BosfqewHDjYRAHtt7IcVA\nWKseYK5TpBZLIbEK2ZcQLo65huyuKh6z4OReqjCvpQ==\n", 'content' => "NQSRqhjkHyUyT00YDgQ6WayC0EwtdytQ/iZAUdc6mnXKNQc74hae3VIJqjUf\nNkq1Qus/dS7a5DMsKa+2F74p1UF3YbswNnZ7fc2VH+iz6hIdYbgCu5bmAuVF\nXFpXk7Nw9n4Cpvjg2x5jxBQ0S1BnYY8J+EBluzichoqub4EVl5yxTWipqiaZ\nKwR1gEEKi3h5w2uljOib62gFdxUnXLtMjDlIXJjN6Kl9idCiz3upBj+4KZFx\n4qBWpvU/6xQRgbbOwfHNzTDl9RSlWSYTXvJ1/7iXYfoSEOMGJSmlGSm/P3Y/\nVPoChK0lfJYanNW4Gu4qQf9d09E5d6NQcGZdN3PNIQ==\n"  },
-        { 'last_edited_by_id' => 2, 'signature' => "GYIflo5hjcsEMjTZxnb4gsKV8bOe2XiG88u8IJ5zvJm3ZavLNmeDJ0PCKkpS\nM05NLLzbaqlGyM7Bh7TubanuvWEi412p9qyMABqVnka3co7GWE3N0RHX1JbG\ncq/O86iO3GDCA7YEwZUjdP0UhI+ytA6WNPU/hsmsDJJzaxXs5GFc51XZUBx9\nHYqKc60i1dL1UIi4IlfcvkiBfUg2lXaeJvHjj2zb0i/khpQ6q0H/OPcNE1Vn\nvYgZGN6PLy60KnirPltKynw8fii5FF2QSntINiC5GM64Eir0ZAKyh9lOQWUV\nCQvtoX4MbUXYWIZXt2sc4GzJE74mPmZ0qmD16hT/kA==\n", 'content' => "gQUlhs+uPz93UN/Cpcd7x8ochDXWbfiC6IrP7ijCfxaa9kvsVnQddYkJVgH1\nBLO6SC36b/lVsGfcvUwmO0vefGrSy2XykXVJdYPMf/LQsOukOs1rLVacg4B6\nf0KeKvLVf/HVYcKiY4L2rFIPvuWC+bUWxRThpiABSu1ZCH9qgcR24PPRnwYw\nsTT15OBHmBzhsP+b3NZI9Yh3OIzWLvDSjQmRuldryuUstPsgfWKd/xawPU4D\nJ8EUFGNltFqb+R4n2eGpNSVQCl/+o9gfMG6dq5CE5HRHwpKgXaybdy753lrF\nHWXS4g6apO4lpJS7kXdhHX7OTbA79GHj/ait9fuZLA==\n" }
-      ],
-      'users' => [{
-        'id' => 1,
-        'username' => 'server',
-        'email' => 'server@localhost',
-        'public_key' => server_public_key.to_s,
-        'url' => 'https://example.com/users/1'
-      }, {
-        'id' => 2,
-        'username' => 'flower-pot',
-        'email' => 'flower-pot@example.org',
-        'public_key' => user_public_key.to_s,
-        'url' => 'https://example.com/users/2'
-      }],
-      'url' => 'http://example.com/secrets/1'
-    }.to_json
-
-    stub_post('/secrets').body(payload).activate
+    stub_post('/secrets').body(secret_hash).activate
   end
 end
 
